885420e80c0e021f5725062716829fdfaf88cdd8
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-19 12:51:45 +0545

    Add Faq data

--- src/components/Home/FAQ/FAQ.jsx
+++ src/components/Home/FAQ/FAQ.jsx
@@ -1,74 +1,56 @@
-import "./Faq.css"
+import { useState } from "react";
+import "./Faq.css";
+import { faqData } from "../../../data/faq/faq";
 
 export default function FAQ() {
-    return (
-        <section className="faq-section" id="faq">
-        <div className="container">
-          <div className="section-heading-container faq-heading">
-            <h6 className="primary-heading">FAQ</h6>
-            <h2 className="sub-heading">
-              Frequently <span className="heading-span">Asked Questions</span>
-            </h2>
-            <p className="heading-para">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vulputate libero et <br />
-              velit interdum, ac aliquet odio mattis.
-            </p>
+
+
+  // State to track which FAQ is open
+  const [openFaqId, setOpenFaqId] = useState(null);
+
+  // Toggle function
+  const toggleFaq = (id) => {
+    setOpenFaqId(openFaqId === id ? null : id);
+  };
+
+  return (
+    <section className="faq-section" id="faq">
+      <div className="container">
+        <div className="section-heading-container faq-heading">
+          <h6 className="primary-heading">FAQ</h6>
+          <h2 className="sub-heading">
+            Frequently <span className="heading-span">Asked Questions</span>
+          </h2>
+          <p className="heading-para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            vulputate libero et <br />
+            velit interdum, ac aliquet odio mattis.
+          </p>
+        </div>
+        <div className="faq-container">
+          <div className="faq-list">
+            {faqData.map((faq) => (
+              <div className="faq-grid" key={faq.id}>
+                <div 
+                  className={`faq-question ${openFaqId === faq.id ? "active" : ""}`}
+                  onClick={() => toggleFaq(faq.id)}
+                >
+                  <p>{faq.question}</p>
+                  <div className="cross-icons">
+                    <i className={`fa-solid ${openFaqId === faq.id ? "fa-minus" : "fa-plus"}`}></i>
+                  </div>
+                </div>
+                <div className={`faq-answer ${openFaqId === faq.id ? "show" : ""}`}>
+                  {faq.answer}
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="faq-container">
-            <div className="faq-list">
-              <div className="faq-grid">
-                <div className="faq-question">
-                  <p>
-                    Gorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                  <div className="cross-icons">
-                    <i className="fa-solid fa-plus"></i>
-                  </div>
-                </div>
-                <div className="faq-answer">
-                  Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                  vulputate libero et velit interdum, ac aliquet odio mattis.
-                  className aptent taciti sociosqu ad litora torquent per conubia
-                  nostra, per inceptos himenaeos.
-                </div>
-              </div>
-              <div className="faq-grid">
-                <div className="faq-question">
-                  <p>
-                    Gorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                  <div className="cross-icons">
-                    <i className="fa-solid fa-plus"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="faq-grid">
-                <div className="faq-question">
-                  <p>
-                    Gorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                  <div className="cross-icons">
-                    <i className="fa-solid fa-plus"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="faq-grid">
-                <div className="faq-question">
-                  <p>
-                    Gorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                  <div className="cross-icons">
-                    <i className="fa-solid fa-plus"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="faq-img">
-              <img src="/faq-img.jpg" alt="faq-img" className="img-faq" />
-            </div>
+          <div className="faq-img">
+            <img src="/faq-img.jpg" alt="faq-img" className="img-faq" />
           </div>
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  );
 }
---
+++ src/data/faq/faq.js
@@ -0,0 +1,22 @@
+export const faqData = [
+  {
+    id: 1,
+    question: "Gorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    answer: "Torem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+  },
+  {
+    id: 2,
+    question: "Consectetur adipiscing elit gorem ipsum dolor sit amet?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+  },
+  {
+    id: 3,
+    question: "Nunc vulputate libero et velit interdum adipiscing elit?",
+    answer: "Dorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+  },
+  {
+    id: 4,
+    question: "Ac aliquet odio mattis class aptent taciti sociosqu?",
+    answer: "Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+  }
+];
