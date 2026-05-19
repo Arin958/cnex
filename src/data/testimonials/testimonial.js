903d350290cdf71d54c6f9972f05dae227ca2cdd
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-19 12:11:49 +0545

    Fixing img issues

--- src/data/testimonials/testimonial.js
+++ src/data/testimonials/testimonial.js
@@ -11,8 +11,8 @@ export const testimonials = [
   },
   {
     id: 2,
-    image: "/testimonial-2.jpg",
-    profileImg: "/testimonial-profile-2.png",
+    image: "/testimonial-1.jpg",
+    profileImg: "/testimonial-profile.png",
     comment: "“Excellent service and professional team”",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
     name: "SARAH JOHNSON",
@@ -21,8 +21,8 @@ export const testimonials = [
   },
   {
     id: 3,
-    image: "/testimonial-3.jpg",
-    profileImg: "/testimonial-profile-3.png",
+    image: "/testimonial-1.jpg",
+    profileImg: "/testimonial-profile.png",
     comment: "“Best experience with this company”",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
     name: "MICHAEL BROWN",
@@ -31,8 +31,8 @@ export const testimonials = [
   },
   {
     id: 4,
-    image: "/testimonial-4.jpg",
-    profileImg: "/testimonial-profile-4.png",
+    image: "/testimonial-1.jpg",
+    profileImg: "/testimonial-profile.png",
     comment: "“High quality work and timely delivery”",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
     name: "EMILY DAVIS",
