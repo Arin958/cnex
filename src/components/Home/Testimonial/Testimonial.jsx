134db705a5e918bb89b8778de33144321225e156
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-19 11:48:13 +0545

    Add a dummy data for testimonial

--- src/components/Home/Testimonial/Testimonial.jsx
+++ src/components/Home/Testimonial/Testimonial.jsx
@@ -1,200 +1,122 @@
-import "./Testimonial.css"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation } from 'swiper/modules';
+
+import "./Testimonial.css";
+import { testimonials } from '../../../data/testimonials/testimonial';
 
 export default function Testimonial() {
-    return (
-          <section className="testimonial-section section" id="testimonial">
-        <div className="container">
-          <div className="section-heading-container testimonial-heading">
-            <div className="heading-container">
-              <h6 className="primary-heading">Testimonial</h6>
-              <h2 className="sub-heading">
-                What our <span className="heading-span">Customer Say</span>
-              </h2>
-            </div>
-            <div className="swiper-button-container">
-              <div className="swiper-button-next">
-                <i className="fa-solid fa-arrow-right"></i>
-              </div>
-              <div className="swiper-button-prev">
-                <i className="fa-solid fa-arrow-left"></i>
-              </div>
-            </div>
+
+  return (
+    <section className="testimonial-section section" id="testimonial">
+      <div className="container">
+        <div className="section-heading-container testimonial-heading">
+          <div className="heading-container">
+            <h6 className="primary-heading">Testimonial</h6>
+            <h2 className="sub-heading">
+              What our <span className="heading-span">Customer Say</span>
+            </h2>
           </div>
-          <div className="testimonial-container">
-            <div className="swiper mySwiper">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <div className="slide-container">
-                    <div className="slide-img">
-                      <img
-                        src="/testimonial-1.jpg"
-                        alt="testimonial-1"
-                        className="slider-img"
-                        width="496px"
-                      />
-                    </div>
-
-                    <div className="slide-content">
-                      <div className="profile-img">
-                        <img
-                          src="/testimonial-profile.png"
-                          alt="profile-img"
-                          width="38px"
-                          height="45px"
-                        />
-                      </div>
-                      <div className="comment">
-                        <div className="comment-container">
-                          <p className="slide-paragraph">
-                            “They did an amazing work for our home”
-                          </p>
-
-                          <p className="slide-para">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Voluptatem earum assumenda, voluptas
-                            exercitationem animi nemo.
-                          </p>
-                        </div>
-
-                        <div className="address">
-                          <p className="name-address primary-heading">
-                            JOHN CARTER, NEW YORK
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="slide-container">
-                    <div className="slide-img">
-                      <img
-                        src="/testimonial-1.jpg"
-                        alt="testimonial-1"
-                        className="slider-img"
-                        width="496px"
-                      />
-                    </div>
-
-                    <div className="slide-content">
-                      <div className="profile-img">
-                        <img
-                          src="/testimonial-profile.png"
-                          alt="profile-img"
-                          width="38px"
-                          height="45px"
-                        />
-                      </div>
-                      <div className="comment">
-                        <div className="comment-container">
-                          <p className="slide-paragraph">
-                            “They did an amazing work for our home”
-                          </p>
-
-                          <p className="slide-para">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Voluptatem earum assumenda, voluptas
-                            exercitationem animi nemo.
-                          </p>
-                        </div>
-
-                        <div className="address">
-                          <p className="name-address primary-heading">
-                            JOHN CARTER, NEW YORK
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="slide-container">
-                    <div className="slide-img">
-                      <img
-                        src="/testimonial-1.jpg"
-                        alt="testimonial-1"
-                        className="slider-img"
-                        width="496px"
-                      />
-                    </div>
-
-                    <div className="slide-content">
-                      <div className="profile-img">
-                        <img
-                          src="/testimonial-profile.png"
-                          alt="profile-img"
-                          width="38px"
-                          height="45px"
-                        />
-                      </div>
-                      <div className="comment">
-                        <div className="comment-container">
-                          <p className="slide-paragraph">
-                            “They did an amazing work for our home”
-                          </p>
-
-                          <p className="slide-para">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Voluptatem earum assumenda, voluptas
-                            exercitationem animi nemo.
-                          </p>
-                        </div>
-
-                        <div className="address">
-                          <p className="name-address primary-heading">
-                            JOHN CARTER, NEW YORK
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="slide-container">
-                    <div className="slide-img">
-                      <img
-                        src="/testimonial-1.jpg"
-                        alt="testimonial-1"
-                        className="slider-img"
-                        width="496px"
-                      />
-                    </div>
-
-                    <div className="slide-content">
-                      <div className="profile-img">
-                        <img
-                          src="/testimonial-profile.png"
-                          alt="profile-img"
-                          width="38px"
-                          height="45px"
-                        />
-                      </div>
-                      <div className="comment">
-                        <div className="comment-container">
-                          <p className="slide-paragraph">
-                            “They did an amazing work for our home”
-                          </p>
-
-                          <p className="slide-para">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Voluptatem earum assumenda, voluptas
-                            exercitationem animi nemo.
-                          </p>
-                        </div>
-
-                        <div className="address">
-                          <p className="name-address primary-heading">
-                            JOHN CARTER, NEW YORK
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="swiper-button-container">
+            <div className="swiper-button-next">
+              <i className="fa-solid fa-arrow-right"></i>
+            </div>
+            <div className="swiper-button-prev">
+              <i className="fa-solid fa-arrow-left"></i>
             </div>
           </div>
         </div>
-      </section>
-    )
+        <div className="testimonial-container">
+          <Swiper
+            slidesPerView={'auto'}
+            spaceBetween={30}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            modules={[Navigation]}
+            grabCursor={true}
+            touchRatio={1.2}
+            resistanceRatio={0.85}
+            simulateTouch={true}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 24,
+              },
+              768: {
+                slidesPerView: 1,
+                spaceBetween: 24,
+                centeredSlides: false,
+              },
+              900: {
+                slidesPerView: 2,
+                spaceBetween: 28,
+              },
+              1100: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+              },
+              1280: {
+                slidesPerView: 2,
+                spaceBetween: 32,
+              },
+            }}
+            className="mySwiper"
+          >
+            {testimonials.map((testimonial) => (
+              <SwiperSlide key={testimonial.id}>
+                <div className="slide-container">
+                  <div className="slide-img">
+                    <img
+                      src={testimonial.image}
+                      alt={`testimonial-${testimonial.id}`}
+                      className="slider-img"
+                      width="496px"
+                    />
+                  </div>
+
+                  <div className="slide-content">
+                    <div className="profile-img">
+                      <img
+                        src={testimonial.profileImg}
+                        alt="profile-img"
+                        width="38px"
+                        height="45px"
+                      />
+                    </div>
+                    <div className="comment">
+                      <div className="comment-container">
+                        <p className="slide-paragraph">
+                          {testimonial.comment}
+                        </p>
+
+                        <p className="slide-para">
+                          {testimonial.description}
+                        </p>
+                      </div>
+
+                      <div className="address">
+                        <p className="name-address primary-heading">
+                          {testimonial.name}, {testimonial.location}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ src/data/testimonials/testimonial.js
@@ -0,0 +1,42 @@
+export const testimonials = [
+  {
+    id: 1,
+    image: "/testimonial-1.jpg",
+    profileImg: "/testimonial-profile.png",
+    comment: "“They did an amazing work for our home”",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
+    name: "JOHN CARTER",
+    location: "NEW YORK",
+    rating: 5
+  },
+  {
+    id: 2,
+    image: "/testimonial-2.jpg",
+    profileImg: "/testimonial-profile-2.png",
+    comment: "“Excellent service and professional team”",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
+    name: "SARAH JOHNSON",
+    location: "LOS ANGELES",
+    rating: 5
+  },
+  {
+    id: 3,
+    image: "/testimonial-3.jpg",
+    profileImg: "/testimonial-profile-3.png",
+    comment: "“Best experience with this company”",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
+    name: "MICHAEL BROWN",
+    location: "CHICAGO",
+    rating: 4
+  },
+  {
+    id: 4,
+    image: "/testimonial-4.jpg",
+    profileImg: "/testimonial-profile-4.png",
+    comment: "“High quality work and timely delivery”",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem earum assumenda, voluptas exercitationem animi nemo.",
+    name: "EMILY DAVIS",
+    location: "MIAMI",
+    rating: 5
+  }
+];
